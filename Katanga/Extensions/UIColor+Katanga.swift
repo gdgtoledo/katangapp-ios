26b6086d4f87a29aec2f2ec26f1ff0a2790c49d9
Victor Galan <gagranta@gmail.com>
2016-10-09 19:49:51 +0200

    Color extension

---
+++ Katanga/Extensions/UIColor+Katanga.swift
@@ -0,0 +1,16 @@
+//
+//  Katanga+UIColor.swift
+//  Katanga
+//
+//  Created by Víctor Galán on 9/10/16.
+//  Copyright © 2016 Software Craftsmanship Toledo. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    open class var katangaYellow: UIColor {
+        return UIColor(red: 246/255.0, green: 236/255.0, blue: 94/255.0, alpha: 1.0)
+    }
+}
