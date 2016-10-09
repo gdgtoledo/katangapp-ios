dffbd77ef774ec501973fb3b97f6fa7bf95d99a3
Victor Galan <gagranta@gmail.com>
2016-10-09 18:08:21 +0200

    Rename class

---
+++ Katanga/InitialViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Katanga
+//
+//  Created by Víctor Galán on 9/10/16.
+//  Copyright © 2016 Software Craftsmanship Toledo. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
