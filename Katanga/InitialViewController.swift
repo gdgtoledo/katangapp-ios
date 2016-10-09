6ee027cdc68075ca925de8c663d7b9a0fb7a5aa3
Victor Galan <gagranta@gmail.com>
2016-10-09 19:52:19 +0200

    Initial view controller UI

--- Katanga/InitialViewController.swift
+++ Katanga/InitialViewController.swift
@@ -7,12 +7,49 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class InitialViewController: UIViewController {
+    
+    var disposeBag = DisposeBag()
+    
+    var spinner: UIActivityIndicatorView?
+    
+    @IBOutlet weak var searchLocationButton: UIButton! {
+        didSet {
+            searchLocationButton.backgroundColor = .katangaYellow
+            searchLocationButton.layer.cornerRadius = 30
+            
+            spinner = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+            spinner!.color = .black
+            spinner!.center = CGPoint(x: 30, y: 30)
+
+            searchLocationButton.addSubview(spinner!)
+        }
+    }
+
+    @IBOutlet weak var metersSlider: UISlider!
+    @IBOutlet weak var metersLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUpRx()
+    }
+    
+    private func setUpRx() {
+        metersSlider.rx.value
+            .startWith(500)
+            .map {"\(Int($0))"}
+            .bindTo(metersLabel.rx.text)
+            .addDisposableTo(disposeBag)
+        
+        searchLocationButton.rx.tap
+            .bindNext {
+                self.spinner?.startAnimating()
+            }
+            .addDisposableTo(disposeBag)
     }
 }
 
