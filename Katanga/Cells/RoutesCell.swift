129219d4172fa6890c168d85eeaeaa80625c82c1
Victor Galan <gagranta@gmail.com>
2016-10-23 00:05:07 +0200

    Add cell for routes

---
+++ Katanga/Cells/RoutesCell.swift
@@ -0,0 +1,54 @@
+//
+//  RoutesCell.swift
+//  Katanga
+//
+//  Created by Víctor Galán on 22/10/16.
+//  Copyright © 2016 Software Craftsmanship Toledo. All rights reserved.
+//
+
+import UIKit
+
+class RoutesCell: UITableViewCell {
+    
+    
+    //MARK: Public variables
+    
+    public var routeId: String {
+        set {
+            routeIdLabel.text = newValue
+        }
+        get {
+            return routeIdLabel.text ?? ""
+        }
+    }
+    
+    public var routeName: String {
+        set {
+            routeNameLabel.text = newValue
+        }
+        get {
+            return routeNameLabel.text ?? ""
+        }
+    }
+    
+    
+    //MARK: Outlets
+    
+    @IBOutlet private weak var containerView: UIView! {
+        didSet {
+            containerView.layer.cornerRadius = 10
+            containerView.layer.masksToBounds = true
+        }
+    }
+    
+    @IBOutlet private weak var routeIdLabel: UILabel!
+    @IBOutlet private weak var routeNameLabel: UILabel!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        routeIdLabel.text = ""
+        routeNameLabel.text = ""
+    }
+    
+}
