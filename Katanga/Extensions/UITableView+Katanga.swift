550502787e928b9fc5ac9dc19d9a6683b827a61c
Victor Galan <gagranta@gmail.com>
2016-10-23 14:14:06 +0200

    Wrapper for reactive method in order to avoid passing cell identifier

--- Katanga/Extensions/UITableView+Katanga.swift
+++ Katanga/Extensions/UITableView+Katanga.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
-
+import RxSwift
+import RxCocoa
 
 extension UITableView {
     
     func register<T: UITableViewCell>(_ : T.Type) where T: NibLoadableView, T: ReusableView {
         let nib = UINib(nibName: T.nibName, bundle: nil)
         register(nib, forCellReuseIdentifier: T.reuseIdentifier)
+    }
+}
+
+
+extension Reactive where Base : UITableView {
+    
+    func items<S: Sequence, Cell: UITableViewCell, O : ObservableType>
+        (cellType: Cell.Type = Cell.self)
+        -> (_ source: O)
+        -> (_ configureCell: @escaping (Int, S.Iterator.Element, Cell) -> Void)
+        -> Disposable
+        where O.E == S, Cell : ReusableView {
+            
+        return items(cellIdentifier: cellType.reuseIdentifier, cellType: cellType)
     }
 }
