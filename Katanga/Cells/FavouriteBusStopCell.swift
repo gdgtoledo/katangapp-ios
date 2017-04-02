2b999e16167d781669ddb7788ff04342e4c6599a
Víctor Galán <gagranta@gmail.com>
2017-04-02 18:13:33 +0200

    Add favorite cell

---
+++ Katanga/Cells/FavouriteBusStopCell.swift
@@ -0,0 +1,33 @@
+//
+//  FavouriteBusStopCell.swift
+//  Katanga
+//
+//  Created by Víctor Galán on 2/4/17.
+//  Copyright © 2017 Software Craftsmanship Toledo. All rights reserved.
+//
+
+import UIKit
+
+class FavouriteBusStopCell: UITableViewCell {
+	
+	var busStopId: String? {
+		set {
+			busStopIdLabel.text = newValue
+		}
+		get {
+			return busStopIdLabel.text ?? ""
+		}
+	}
+	
+	var busStopAddress: String? {
+		set {
+			busStopAddressLabel.text = newValue
+		}
+		get {
+			return busStopAddressLabel.text ?? ""
+		}
+	}
+
+	@IBOutlet weak var busStopIdLabel: UILabel!
+	@IBOutlet weak var busStopAddressLabel: UILabel!
+}
